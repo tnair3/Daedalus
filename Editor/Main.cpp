49ebba445ac51a8c025f914c0c59601494a1846e
tnair3 <nairstejas@gmail.com>
2026-09-20 23:59:16 +0100

    Editor UI changes, with (mostly) unimplemented menu bar items

--- Editor/Main.cpp
+++ Editor/Main.cpp
@@ -206,8 +206,20 @@ namespace Editor {
         while (m_IsRunning && !m_Window.ShouldClose())
         {
             m_Window.PollEvents();
-            m_engine.Render();
+
             BeginFrame();
+
+            bool resetLayoutRequested = false;
+
+            static bool showProjectExplorer = true;
+            static bool showSceneViewer = true;
+            static bool showInspector = true;
+            static bool showCodeEditor = true;
+            static bool showTerminal = true;
+            static bool showVersionControl = true;
+
+            static bool showMetricsWindow = false;
+            static bool showDemoWindow = false;
 
             // Menu bar
             if (ImGui::BeginMainMenuBar())
@@ -228,17 +240,31 @@ namespace Editor {
                             m_engine.LoadProject(selectedPath);
                             NFD::FreePath(outPath);
                         }
-                        else if (result == NFD_CANCEL)
-                        {
-                            std::cout << "User canceled file selection." << std::endl;
-                        }
+                        else if (result == NFD_CANCEL) {  }
                         else
                         {
                             std::cerr << "Error: " << NFD::GetError() << std::endl;
                         }
                     }
-                    if (ImGui::MenuItem("Save", "Ctrl+S")) {  }
+                    if (ImGui::BeginMenu("Recent Projects"))
+                    {
+                        if (ImGui::MenuItem("Test Item 1")) {  }
+                        if (ImGui::MenuItem("Test Item 2")) {  }
+                        ImGui::EndMenu();
+                    }
                     ImGui::Separator();
+
+                    if (ImGui::MenuItem("New Scene", "Ctrl+Shift+N")) {  }
+                    if (ImGui::MenuItem("Open Scene", "Ctrl+Shift+O")) {  }
+                    if (ImGui::MenuItem("Save Scene", "Ctrl+S")) {  }
+                    if (ImGui::MenuItem("Save Scene As", "Ctrl+Shift+S")) {  }
+                    if (ImGui::MenuItem("Save All", "Ctrl+Alt+S")) {  }
+                    ImGui::Separator();
+
+                    if (ImGui::MenuItem("Build Settings", "Ctrl+Shift+B")) {  }
+                    if (ImGui::MenuItem("Export Asset")) {  }
+                    ImGui::Separator();
+
                     if (ImGui::MenuItem("Exit", "Alt+F4")) { m_IsRunning = false; }
                     ImGui::EndMenu();
                 }
@@ -247,16 +273,136 @@ namespace Editor {
                 {
                     if (ImGui::MenuItem("Undo", "Ctrl+Z")) {  }
                     if (ImGui::MenuItem("Redo", "Ctrl+Y")) {  }
+                    ImGui::Separator();
+
                     if (ImGui::MenuItem("Cut", "Ctrl+X")) {  }
                     if (ImGui::MenuItem("Copy", "Ctrl+C")) {  }
                     if (ImGui::MenuItem("Paste", "Ctrl+V")) {  }
+                    if (ImGui::MenuItem("Duplicate", "Ctrl+D")) {  }
+                    if (ImGui::MenuItem("Delete", "Del")) {  }
+                    ImGui::Separator();
+
+                    if (ImGui::MenuItem("Select All", "Ctrl+A")) {  }
+                    if (ImGui::MenuItem("Deselect All", "Escape")) {  }
+                    ImGui::Separator();
+
+                    if (ImGui::MenuItem("Project Settings")) {  } // Open a dedicated window for input, graphics, audio, physics settings
+                    if (ImGui::MenuItem("Editor Preferences")) {  } // Open a dedicated window for theme, keybindings, layout defaults
                     ImGui::EndMenu();
                 }
 
                 if (ImGui::BeginMenu("View"))
                 {
+                    if (ImGui::BeginMenu("Layouts"))
+                    {
+                        if (ImGui::MenuItem("Default Layout")) { resetLayoutRequested = true; }
+                        if (ImGui::MenuItem("Scripting Layout")) {  }
+                        ImGui::EndMenu();
+                    }
+                    if (ImGui::BeginMenu("Panels"))
+                    {
+                        ImGui::MenuItem("Project Explorer", nullptr, &showProjectExplorer);
+                        ImGui::MenuItem("Scene Viewer", nullptr, &showSceneViewer);
+                        ImGui::MenuItem("Inspector", nullptr, &showInspector);
+                        ImGui::MenuItem("Code Editor", nullptr, &showCodeEditor);
+                        ImGui::MenuItem("Terminal / Logs", nullptr, &showTerminal);
+                        ImGui::MenuItem("Version Control", nullptr, &showVersionControl);
+                        ImGui::EndMenu();
+                    }
+                    ImGui::Separator();
+
+                    if (ImGui::MenuItem("Toggle Fullscreen", "F11")) {  }
                     ImGui::EndMenu();
                 }
+
+                if (ImGui::BeginMenu("Entity"))
+                {
+                    if (ImGui::MenuItem("Create Empty", "Ctrl+Shift+E")) {  }
+                    ImGui::Separator();
+
+                    if (ImGui::BeginMenu("2D Sprites"))
+                    {
+                        if (ImGui::MenuItem("Square")) {  }
+                        if (ImGui::MenuItem("Circle")) {  }
+                        if (ImGui::MenuItem("Custom Texture")) {  }
+                        ImGui::EndMenu();
+                    }
+                    if (ImGui::BeginMenu("Tilemap"))
+                    {
+                        if (ImGui::MenuItem("Rectangular Tilemap")) {  }
+                        if (ImGui::MenuItem("Isometric Tilemap")) {  }
+                        ImGui::EndMenu();
+                    }
+                    if (ImGui::BeginMenu("Lights"))
+                    {
+                        if (ImGui::MenuItem("Point Light")) {  }
+                        if (ImGui::MenuItem("Global / Ambient Light")) {  }
+                        if (ImGui::MenuItem("Spotlight")) {  }
+                        ImGui::EndMenu();
+                    }
+                    if (ImGui::MenuItem("2D Camera")) {  }
+                    if (ImGui::MenuItem("Audio Source")) {  }
+                    ImGui::EndMenu();
+                }
+
+                if (ImGui::BeginMenu("Component"))
+                {
+                    if (ImGui::BeginMenu("Rendering"))
+                    {
+                        if (ImGui::MenuItem("Sprite Renderer")) {  }
+                        if (ImGui::MenuItem("Tilemap Renderer")) {  }
+                        if (ImGui::MenuItem("Sorting Group")) {  }
+                        ImGui::EndMenu();
+                    }
+                    if (ImGui::BeginMenu("2D Physics"))
+                    {
+                        if (ImGui::MenuItem("RigidBody 2D")) {  }
+                        if (ImGui::MenuItem("Box Collider 2D")) {  }
+                        if (ImGui::MenuItem("Circle Collider 2D")) {  }
+                        if (ImGui::MenuItem("Polygon Collider 2D")) {  }
+                        ImGui::EndMenu();
+                    }
+                    if (ImGui::BeginMenu("Animation"))
+                    {
+                        if (ImGui::MenuItem("Animator 2D")) {  }
+                        if (ImGui::MenuItem("Sprite Sheet Player")) {  }
+                        ImGui::EndMenu();
+                    }
+                    if (ImGui::BeginMenu("Audio"))
+                    {
+                        if (ImGui::MenuItem("Audio Source 2D")) {  }
+                        if (ImGui::MenuItem("Audio Listener")) {  }
+                        ImGui::EndMenu();
+                    }
+                    if (ImGui::BeginMenu("Scripts"))
+                    {
+                        if (ImGui::MenuItem("Attach Script")) {  }
+                        ImGui::EndMenu();
+                    }
+                    ImGui::EndMenu();
+                }
+
+                if (ImGui::BeginMenu("Tools"))
+                {
+                    if (ImGui::MenuItem("Tile Palette Editor")) {  }
+                    if (ImGui::MenuItem("Sprite Editor")) {  }
+                    ImGui::Separator();
+
+                    if (ImGui::MenuItem("Recompile Scripts / Shaders", "F5")) {  }
+                    ImGui::MenuItem("ImGui Metrics", nullptr, &showMetricsWindow);
+                    ImGui::EndMenu();
+                }
+
+                if (ImGui::BeginMenu("Help"))
+                {
+                    if (ImGui::MenuItem("Engine Documentation")) {  }
+                    ImGui::MenuItem("ImGui Demo Window", nullptr, &showDemoWindow);
+                    ImGui::Separator();
+
+                    if (ImGui::MenuItem("About Daedalus Engine")) {  }
+                    ImGui::EndMenu();
+                }
+
                 ImGui::EndMainMenuBar();
             }
 
@@ -267,31 +413,37 @@ namespace Editor {
             ImGui::SetNextWindowViewport(viewport->ID);
 
             ImGuiWindowFlags hostWindowFlags =
-                ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse |
-                ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove |
-                ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus |
-                ImGuiWindowFlags_NoDocking;
+                        ImGuiWindowFlags_NoTitleBar | ImGuiWindowFlags_NoCollapse |
+                        ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove |
+                        ImGuiWindowFlags_NoBringToFrontOnFocus | ImGuiWindowFlags_NoNavFocus |
+                        ImGuiWindowFlags_NoDocking | ImGuiWindowFlags_NoBackground;
 
             ImGui::PushStyleVar(ImGuiStyleVar_WindowRounding, 0.0f);
             ImGui::PushStyleVar(ImGuiStyleVar_WindowBorderSize, 0.0f);
+            ImGui::PushStyleVar(ImGuiStyleVar_WindowPadding, ImVec2(0.0f, 0.0f));
 
             ImGui::Begin("EditorRootDockSpaceWindow", nullptr, hostWindowFlags);
-            ImGui::PopStyleVar(2);
+            ImGui::PopStyleVar(3);
 
             ImGuiID dockSpace_id = ImGui::GetID("EditorDockSpace");
-            ImGui::DockSpace(dockSpace_id, ImVec2(0.0f, 0.0f), ImGuiDockNodeFlags_None);
 
-            static bool layoutInitialized = false;
-            if (!layoutInitialized)
+            if (ImGui::DockBuilderGetNode(dockSpace_id) == nullptr || resetLayoutRequested)
             {
-                layoutInitialized = true;
+                showProjectExplorer = true;
+                showSceneViewer = true;
+                showInspector = true;
+                showCodeEditor = true;
+                showTerminal = true;
+                showVersionControl = true;
+
+                showMetricsWindow = false;
+                showDemoWindow = false;
 
                 ImGui::DockBuilderRemoveNode(dockSpace_id);
                 ImGui::DockBuilderAddNode(dockSpace_id, ImGuiDockNodeFlags_DockSpace);
                 ImGui::DockBuilderSetNodeSize(dockSpace_id, viewport->WorkSize);
 
                 ImGuiID dockIdCenter;
-
                 ImGuiID dockIdLeft = ImGui::DockBuilderSplitNode(dockSpace_id, ImGuiDir_Left, 0.20f, nullptr, &dockIdCenter);
                 ImGuiID dockIdRight = ImGui::DockBuilderSplitNode(dockIdCenter, ImGuiDir_Right, 0.25f, nullptr, &dockIdCenter);
 
@@ -308,61 +460,86 @@ namespace Editor {
 
                 ImGui::DockBuilderDockWindow("Project Explorer", dockIdLeftTop);
                 ImGui::DockBuilderDockWindow("Version Control", dockIdLeftBottom);
-
                 ImGui::DockBuilderDockWindow("Scene Viewer", dockIdCenterTop);
                 ImGui::DockBuilderDockWindow("Code Editor", dockIdCenterTop);
                 ImGui::DockBuilderDockWindow("Terminal Output", dockIdCenterBottom);
-
                 ImGui::DockBuilderDockWindow("Inspector", dockIdRight);
 
                 ImGui::DockBuilderFinish(dockSpace_id);
             }
+
+            ImGuiDockNodeFlags dockspaceFlags = ImGuiDockNodeFlags_NoWindowMenuButton;
+
+            ImGui::DockSpace(dockSpace_id, ImVec2(0.0f, 0.0f), dockspaceFlags);
             ImGui::End();
 
-            ImGui::Begin("Project Explorer");
-            ImGui::Text("File hierarchy tree goes here.");
-            ImGui::End();
-
-            ImGui::Begin("Version Control");
-            ImGui::Text("Git history logs.");
-            ImGui::End();
-
-            ImGui::Begin("Scene Viewer");
-            if (const auto& offscreenTarget = m_engine.GetRenderTarget(); offscreenTarget.ImageView != VK_NULL_HANDLE && offscreenTarget.Sampler != VK_NULL_HANDLE)
+            if (showProjectExplorer)
             {
-                if (m_CurrentViewportImageView != offscreenTarget.ImageView)
+                ImGui::Begin("Project Explorer");
+                ImGui::Text("File hierarchy tree goes here.");
+                ImGui::End();
+            }
+
+            if (showVersionControl)
+            {
+                ImGui::Begin("Version Control");
+                ImGui::Text("Git history logs.");
+                ImGui::End();
+            }
+
+            if (showSceneViewer)
+            {
+                ImGui::Begin("Scene Viewer");
+                if (const auto& offscreenTarget = m_engine.GetRenderTarget(); offscreenTarget.ImageView != VK_NULL_HANDLE && offscreenTarget.Sampler != VK_NULL_HANDLE)
                 {
-                    if (m_ViewportTextureID != 0) { ImGui_ImplVulkan_RemoveTexture(reinterpret_cast<VkDescriptorSet>(m_ViewportTextureID)); }
+                    if (m_CurrentViewportImageView != offscreenTarget.ImageView)
+                    {
+                        if (m_ViewportTextureID != 0) { ImGui_ImplVulkan_RemoveTexture(reinterpret_cast<VkDescriptorSet>(m_ViewportTextureID)); }
 
-                    m_ViewportTextureID = reinterpret_cast<ImTextureID>(ImGui_ImplVulkan_AddTexture(
-                        offscreenTarget.Sampler,
-                        offscreenTarget.ImageView,
-                        VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL
-                    ));
+                        m_ViewportTextureID = reinterpret_cast<ImTextureID>(ImGui_ImplVulkan_AddTexture(
+                            offscreenTarget.Sampler,
+                            offscreenTarget.ImageView,
+                            VK_IMAGE_LAYOUT_SHADER_READ_ONLY_OPTIMAL
+                        ));
 
-                    m_CurrentViewportImageView = offscreenTarget.ImageView;
+                        m_CurrentViewportImageView = offscreenTarget.ImageView;
+                    }
+
+                    ImVec2 viewportPanelSize = ImGui::GetContentRegionAvail();
+                    ImGui::Image(m_ViewportTextureID, viewportPanelSize);
                 }
-
-                ImVec2 viewportPanelSize = ImGui::GetContentRegionAvail();
-                ImGui::Image(m_ViewportTextureID, viewportPanelSize);
+                else
+                {
+                    ImGui::Text("Vulkan Viewport Render Target Window (Error in displaying scene)");
+                }
+                ImGui::End();
             }
-            else
+
+            if (showCodeEditor)
             {
-                ImGui::Text("Vulkan Viewport Render Target Window (Error in displaying scene)");
+                ImGui::Begin("Code Editor");
+                ImGui::Text("Built in code editor window");
+                ImGui::End();
             }
-            ImGui::End();
 
-            ImGui::Begin("Code Editor");
-            ImGui::Text("Built in code editor window");
-            ImGui::End();
+            if (showTerminal)
+            {
+                ImGui::Begin("Terminal Output");
+                ImGui::Text("Build output logs go here.");
+                ImGui::End();
+            }
 
-            ImGui::Begin("Terminal Output");
-            ImGui::Text("Build output logs go here.");
-            ImGui::End();
+            if (showInspector)
+            {
+                ImGui::Begin("Inspector");
+                ImGui::Text("Component properties.");
+                ImGui::End();
+            }
 
-            ImGui::Begin("Inspector");
-            ImGui::Text("Component properties.");
-            ImGui::End();
+            if (showMetricsWindow) { ImGui::ShowMetricsWindow(&showMetricsWindow); }
+            if (showDemoWindow) { ImGui::ShowDemoWindow(&showDemoWindow); }
+
+            m_engine.Render();
 
             EndFrame();
         }
